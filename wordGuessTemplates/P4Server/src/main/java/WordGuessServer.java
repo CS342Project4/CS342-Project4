c40c2522689f7bee1eb2baa78c93d4183875afad
Kunj Patel <37160487+kunjpatel49@users.noreply.github.com>
2020-04-20 15:40:56 -0500

    Update WordGuessServer.java

--- wordGuessTemplates/P4Server/src/main/java/WordGuessServer.java
+++ wordGuessTemplates/P4Server/src/main/java/WordGuessServer.java
@@ -1,9 +1,13 @@
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -22,23 +26,18 @@ public class WordGuessServer extends Application {
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		//****
-		serverConnection = new Server(data->{
-			Platform.runLater(()->{
-				serverlog.getItems().add(data.toString());
-			});
-		});
-		//****
+
+		Parent root = FXMLLoader.load(getClass().getResource("fxml/serverLoginScreen.fxml"));
 
 
 		primaryStage.setTitle("(server) Playing word guess!!!");
-		BorderPane bp = new BorderPane();
-		serverlog = new ListView<String>();
-		bp.setCenter(serverlog);
-		bp.setPadding(new Insets(80));
-		bp.setStyle("-fx-background-color: crimson");
-		Scene scene = new Scene(bp,600,600);
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		pane.setHgap(20);
+		pane.setVgap(20);
+		pane.setStyle("-fx-background-color:  #475B5A");
+		Scene scene = new Scene(pane, 723, 440);
+		pane.add(root, 0, 0);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}

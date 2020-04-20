8c3d0a4b6e96e16438a22d10d84d6c9da359a166
Kunj Patel <37160487+kunjpatel49@users.noreply.github.com>
2020-04-20 15:39:42 -0500

    Update ServerInfoScreenController.java

--- wordGuessTemplates/P4Server/src/main/java/ServerInfoScreenController.java
+++ wordGuessTemplates/P4Server/src/main/java/ServerInfoScreenController.java
@@ -1,19 +1,22 @@
-package Controllers;
-
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
-import javax.swing.text.html.ListView;
+import java.io.FileNotFoundException;
+
 
 public class ServerInfoScreenController {
 
     Server serverConnection;
 
     @FXML
-    ListView infoView;
+    ListView serverInfo;
 
-    public void transferPortNumberAndInitializeServer(int port) {
-
-
-
+    public void transferPortNumberAndInitializeServer(int port) throws FileNotFoundException {
+        serverConnection = new Server(
+                data -> Platform.runLater(() -> serverInfo.getItems().add(data.toString())),
+                port
+                );
     }
+
 }

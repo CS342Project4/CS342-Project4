3ce4190b01087c334242cd0cd42dd039aaf4921c
Kunj <kunjpatel49@gmail.com>
2020-04-20 14:38:55 -0500

    added server GUI

---
+++ wordGuessTemplates/P4Server/src/main/java/ServerInfoScreenController.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ListView;
+
+public class ServerInfoScreenController {
+
+    Server serverConnection;
+
+    @FXML
+    ListView infoView;
+
+    public void transferPortNumberAndInitializeServer(int port) {
+
+
+
+    }
+}

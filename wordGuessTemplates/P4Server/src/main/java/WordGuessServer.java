9e8ab519c2c569c09d22bb58cd697e9308ea9336
Kunj Patel <37160487+kunjpatel49@users.noreply.github.com>
2020-04-20 16:23:45 -0500

    Update WordGuessServer.java

--- wordGuessTemplates/P4Server/src/main/java/WordGuessServer.java
+++ wordGuessTemplates/P4Server/src/main/java/WordGuessServer.java
@@ -1,23 +1,13 @@
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
 public class WordGuessServer extends Application {
-	//********
-	ListView<String> serverlog;
-	Server serverConnection;
-	//********
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
@@ -27,7 +17,7 @@ public class WordGuessServer extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		Parent root = FXMLLoader.load(getClass().getResource("fxml/serverLoginScreen.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("serverLoginScreen.fxml"));
 
 
 		primaryStage.setTitle("(server) Playing word guess!!!");
@@ -36,7 +26,7 @@ public class WordGuessServer extends Application {
 		pane.setHgap(20);
 		pane.setVgap(20);
 		pane.setStyle("-fx-background-color:  #475B5A");
-		Scene scene = new Scene(pane, 723, 440);
+		Scene scene = new Scene(pane, 600, 400);
 		pane.add(root, 0, 0);
 		primaryStage.setScene(scene);
 		primaryStage.show();
